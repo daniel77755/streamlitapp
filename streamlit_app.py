9488bce93bfd1d6b7d98b116c3d1e522b70ae926
daniel77755 <31834369+daniel77755@users.noreply.github.com>
2025-04-02 21:08:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,7 @@ with st.form(key='chat_form'):
 if submit_button and query:
     response = connect_api(query)
     data = response
+    st.write(data,"rta")
     #data = data["outputs"][0]["outputs"][0]["results"]["message"]["text"]
     
     if response.status_code == 200:
