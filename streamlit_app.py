8fe5e6eab9913c83aec963315b14ca3df2bfc287
daniel77755 <31834369+daniel77755@users.noreply.github.com>
2025-04-02 20:41:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://api1-zv36.onrender.com/chat"  # Replace with your API URL
+# API_URL = "https://api1-zv36.onrender.com/chat"  # Replace with your API URL
 
 col1, col2 = st.columns([3,1])
 
@@ -50,13 +50,32 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
+def connect_api(query):
+    BASE_API_URL = "https://api.langflow.astra.datastax.com"
+    LANGFLOW_ID = "40e98986-3486-471e-8c5d-6a27d01f3d78"
+    FLOW_ID = "a4bb8b81-8469-4cd6-8898-d31597bed217"
+    APPLICATION_TOKEN = "AstraCS:zwxnTFZtyOUjGKTsnljsrSZc:72bb72b2f2e9336a259fc81e62b194201f59959dc6cf38a766e0af0f9b9a5eb2"
+    headers = {
+        "Authorization": f"Bearer {APPLICATION_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "input_value": query,
+        "output_type": "chat",
+        "input_type": "chat"
+    }
+    
+    return response = requests.post(f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{FLOW_ID}", json=payload, headers=headers, timeout=60)
+    
+
 # Usa st.form para agrupar el input y botón
 with st.form(key='chat_form'):
     query = st.text_input("Realiza las preguntas que hacen tus brokers:")
     submit_button = st.form_submit_button(label='Responder')
 
 if submit_button and query:
-    response = requests.get(API_URL, params={"query": query})
+    response = connect_api(query)
 
     if response.status_code == 200:
         st.write("🔍 Respuesta:", response.json().get("response", "⚠️ No 'response' key found in JSON."))
