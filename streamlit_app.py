6909ae89f9b15490190e4d1b980972ed5d81ac54
daniel77755 <31834369+daniel77755@users.noreply.github.com>
2025-04-02 22:24:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,9 +78,10 @@ with st.form(key='chat_form'):
 if submit_button and query:
     response_api = connect_api(query)
     st.write(response_api)
-    data = response_api.json()
+    
     if response_api.status_code != 200:
         st.write("⚠️ No 'response' key found in JSON.", response_api)
     else:
+        data = response_api.json()
         data = data["outputs"][0]["outputs"][0]["results"]["message"]["text"]
         st.write(data)
