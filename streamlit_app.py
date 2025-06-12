254a25062fc5d1a2d79403ff56304601d9fdbdd8
daniel77755 <31834369+daniel77755@users.noreply.github.com>
2025-06-12 17:01:18 -0500

    Update streamlit_app.py
    data update comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,16 @@ with st.form(key='chat_form'):
 
 with st.expander("¿Cómo realizar una pregunta?"):
     st.markdown("""
-    rrrr
+    Para hacer una pregunta correcta al agente usa la siguiente
+    estructura \n\n
+
+    Explica ("enumera", "dime", …) los
+    requisitos para radicar ( "documentos necesarios", "políticas
+    de legalización", …)  una compra
+    de cartera con BBVA \n\n
+
+    Por último, último no olvides calificar la respuesta
+    obtenida.
     """)      
 
 if submit_button and query:
