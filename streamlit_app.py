b0cde5aa070d5af5bde33db89d9335f3e0eb1bd5
daniel77755 <31834369+daniel77755@users.noreply.github.com>
2025-06-12 16:43:36 -0500

    Update streamlit_app.py
    bug comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,14 +103,7 @@ def connect_api(query):
     
 with st.expander("¿Cómo realizar una pregunta?"):
     st.markdown("""
-    Para hacer una pregunta correcta al agente usa la siguiente
-    estructura \n\n
-    Explica ("enumera", "dime", …) los
-    requisitos para radicar ( "documentos necesarios", "políticas
-    de legalización", …)  una compra
-    de cartera con BBVA \n\n
-    Por último, último no olvides calificar la respuesta
-    obtenida. 
+    rrrr
     """)  
     
 # Usa st.form para agrupar el input y botón 
